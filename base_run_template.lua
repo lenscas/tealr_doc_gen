06723a32a23bb28da147238c9f4fab9ab30f2917
lenscas <lenscas@gmail.com>
2022-08-27 14:34:15 +0200

    generate definition files

--- base_run_template.lua
+++ base_run_template.lua
@@ -1,4 +1,21 @@
 local etlua = etlua()
+
+function inspect_type(name, tbl)
+    if type(tbl) == "table" then
+        for k, v in pairs(tbl) do
+            local typeOf = type(v)
+            if typeOf == "table" then
+                print(name, " has ", k, "of", typeOf, "with")
+                inspect_type(name .. " - " .. k, v)
+            else
+                print(name, " has ", k, "of", typeOf, "containing", v)
+            end
+        end
+    else
+        print(name, " is ", k, "of", typeOf, "containing", tbl)
+    end
+end
+
 local template, err = etlua.compile(template)
 if not template then print(err) end
 return template()
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use app::Paths;
-use run_template::generate_self;
+use run_template::{generate_self_def, generate_self_doc};
 
 use crate::app::get_paths;
 
@@ -18,8 +18,8 @@ fn main() -> anyhow::Result<()> {
             //generate_docs::generate_docs(x)
             run_template::run_template(x)?;
         }
-        app::Modes::SelfDocs { build_dir } => {
-            let walker = generate_self()?;
+        app::Modes::SelfDocTemplate { build_dir } => {
+            let walker = generate_self_doc()?;
             run_template::run_from_walker(
                 Paths {
                     json: "{}".into(),
@@ -27,15 +27,31 @@ fn main() -> anyhow::Result<()> {
                     root: "".into(),
                     build_dir,
                     template_kind: app::TemplateKind::Builtin,
+                    def_config: Default::default(),
                 },
                 walker,
             )?;
         }
+
         app::Modes::GenFile { file, location } => {
             //std::fs::create_dir_all(location)?;
             std::fs::write(location, file)?;
         }
         app::Modes::Nothing => (),
+        app::Modes::SelfDefTemplate { build_dir } => {
+            let walker = generate_self_def()?;
+            run_template::run_from_walker(
+                Paths {
+                    json: "{}".into(),
+                    name: "tealr_doc_gen".into(),
+                    root: "".into(),
+                    build_dir,
+                    template_kind: app::TemplateKind::Builtin,
+                    def_config: Default::default(),
+                },
+                walker,
+            )?;
+        }
     }
     Ok(())
 }
